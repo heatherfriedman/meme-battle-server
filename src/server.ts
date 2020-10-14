a984f6e37ae26cc37b1720b1ea4c0a6d22f6dc8f
Heather Friedman <hfried25@gmail.com>
2020-10-14 10:34:38 -0400

    implemented socket.io

--- src/server.ts
+++ src/server.ts
@@ -8,3 +8,13 @@ const port = process.env.PORT || 3001;
 const server = app.listen(port, () => console.log(`server listening on port ${port}`));
 
 const io = socketIO(server);
+
+io.on('connection', socket => {
+  console.log('a user connected with id:', socket.id);
+  socket.on('disconnect', () => {
+    console.log(`user ${socket.id} disconnected`);
+  });
+  socket.on('set username', name => {
+    console.log(name, 'set as username');
+  });
+});
